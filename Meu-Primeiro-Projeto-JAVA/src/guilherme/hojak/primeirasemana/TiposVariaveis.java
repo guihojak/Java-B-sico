118b4d9575ff8f38514210bd1fd7ac03446600c8
guihojak <guihojak@gmail.com>
2024-03-06 13:38:10 -0300

    Mudanças basicas a mais!

--- Meu-Primeiro-Projeto-JAVA/src/guilherme/hojak/primeirasemana/TiposVariaveis.java
+++ Meu-Primeiro-Projeto-JAVA/src/guilherme/hojak/primeirasemana/TiposVariaveis.java
@@ -1,5 +1,23 @@
 package guilherme.hojak.primeirasemana;
 
 public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 6;
+
+        numero = 10;
+
+        System.out.println(numero);
+        
+        final double VALOR_DE_PI = 3.14;
+
+        
+    }
     
 }
